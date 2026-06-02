694f1ac144738f5184d97874674bb86e5fe8fad4
sanjai201 <727823tuit204@skct.edu.in>
2026-06-02 16:25:42 +0530

    fix mysql config

--- backend/target/generated-sources/annotations/com/budgetplanner/mapper/ExpenseMapperImpl.java
+++ backend/target/generated-sources/annotations/com/budgetplanner/mapper/ExpenseMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-01T13:18:10+0530",
+    date = "2026-06-02T16:16:35+0530",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
